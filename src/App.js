a46dc33e38b11e1cf561f3c68f0e1b277eb9daba
Jose Campo Rivas <josecampor31@gmail.com>
2023-02-23 20:29:25 +0100

    create env file to use API keys

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import SearchInput from './components/SearchInput/SearchInput';
 
+
 function App() {
+  
   return (
     <div className='min-h-screen grid place-items-center bg-slate-800'>
       <SearchInput />
