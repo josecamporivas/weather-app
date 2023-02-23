e6ba9edac722e5b5580eea7710e367669e497ebf
Jose Campo Rivas <josecampor31@gmail.com>
2023-02-23 20:31:08 +0100

    add use-places-autocomplete
    this package provide a easy way to implement a location autocomplete to the search input

--- src/components/SearchInput/SearchInput.jsx
+++ src/components/SearchInput/SearchInput.jsx
@@ -1,22 +1,41 @@
 import { useState } from "react"
+import usePlacesAutocomplete, {
+    getGeocode,
+    getLatLng,
+  } from "use-places-autocomplete";
 
-export default function SearchInput(){
-    const [location, setLocation] = useState('')
+export default function SearchInput(){    
+    const {ready,value,suggestions: { status, data },setValue,clearSuggestions,} = usePlacesAutocomplete({
+        debounce: 200,
+    });
 
-    const handleChange = (e) => {
-        setLocation(e.target.value)
-        console.log(location)
+    const handleChange = async (e) => {
+        setValue(e.target.value)
+        console.log(e.target.value)
+        console.log(data)
+
+        if(data[0] && data[0].description){
+            const results = await getGeocode({
+                address: data[0].description
+            })
+    
+            const { lat, lng } = getLatLng(results[0]);
+            console.log(lat, lng)
+        }
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault()
-        console.log(e)
-        console.log(location)
+        console.log(value)
+
+        //API KEY STILL NOT ACTIVE
+        /* const data = await fetch(URL_EXAMPLE).then(data => data.json())
+        console.log(data) */
     }
 
     return (
         <form onSubmit={handleSubmit}>
-            <input className='bg-slate-200 p-1 rounded placeholder:text-slate-700' placeholder="Search a location..." onChange={handleChange} value={location}/>
+            <input disabled={!ready} className='bg-slate-200 font-semibold p-1 rounded placeholder:text-slate-700' placeholder="Search a location..." onChange={handleChange} value={value}/>        
         </form>
     )
 }
